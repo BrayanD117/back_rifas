96eb94be695ca71cfdcde842271f8f5f33681b9d
BrayanD117 <brayandavid1928@gmail.com>
2024-08-29 16:41:36 -0500

    Add supabase config

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize');
+const config = require('./config');
+
+const environment = process.env.NODE_ENV || 'development';
+const dbConfig = config[environment];
+
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  host: dbConfig.host,
+  dialect: dbConfig.dialect
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos establecida correctamente.');
+  })
+  .catch(err => {
+    console.error('No se pudo conectar a la base de datos:', err);
+  });
+
+module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const sequelize = require('./config/db');
 
 // Middleware
 app.use(express.json());
