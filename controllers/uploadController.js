352901ee33187d34f395ca9f80cd227e9bdb8526
BrayanD117 <brayandavid1928@gmail.com>
2024-10-08 15:39:04 -0500

    Fix in update raffle images

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -9,30 +9,47 @@ exports.handleFileUpload = (req, res) => {
   const files = req.files;
 
   if (!raffleName || !files || files.length === 0) {
-    return res.status(400).json({ message: 'El nombre de la rifa y los archivos son requeridos' });
+    return res
+      .status(400)
+      .json({ message: 'El nombre de la rifa y los archivos son requeridos' });
   }
 
   const sanitizedRaffleName = raffleName.replace(/\s+/g, '_');
 
-  const uploadDir = path.join(__dirname, '..', 'uploads', 'raffles', sanitizedRaffleName);
+  const uploadDir = path.join(
+    __dirname,
+    '..',
+    'uploads',
+    'raffles',
+    sanitizedRaffleName
+  );
 
-  if (fs.existsSync(uploadDir)) {
-    fs.readdirSync(uploadDir).forEach((file) => {
-      fs.unlinkSync(path.join(uploadDir, file));
-    });
-  } else {
+  if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
   }
 
-  files.forEach((file, index) => {
-    console.log('Procesando archivo:', file.originalname);
-    const filePath = path.join(
-      uploadDir,
-      `${sanitizedRaffleName}_${index + 1}${path.extname(file.originalname)}`
-    );
-
+  files.forEach((file) => {
+    const filePath = path.join(uploadDir, file.originalname);
     fs.writeFileSync(filePath, file.buffer);
   });
 
   return res.status(200).json({ message: 'Archivos subidos con éxito' });
 };
+
+exports.deleteFile = (req, res) => {
+  const { imagePath } = req.body;
+
+  if (!imagePath) {
+    return res.status(400).json({ message: 'La ruta de la imagen es requerida' });
+  }
+
+  const filePath = path.join(__dirname, '..', 'uploads', 'raffles', imagePath);
+
+  if (fs.existsSync(filePath)) {
+    fs.unlinkSync(filePath);
+    return res.status(200).json({ message: 'Imagen eliminada con éxito' });
+  } else {
+    return res.status(404).json({ message: 'Imagen no encontrada' });
+  }
+};
+
--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const { handleFileUpload } = require('../controllers/uploadController');
+const uploadController = require('../controllers/uploadController');
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 router.post('/files', upload.array('files', 10), handleFileUpload);
+router.delete('/file', uploadController.deleteFile);
 
 module.exports = router;
